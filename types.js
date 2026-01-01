decde0d4d4414d5efb7bb7aaace1497022084cf0
coder-utshab <ghoshutshob470@gmail.com>
2026-01-01 23:13:21 +0530

    ut

---
+++ types.js
@@ -0,0 +1,26 @@
+// string
+var name = "utshab kumar ghosh";
+console.log(name);
+var address = "Magura";
+console.log(address);
+
+var country = 'Bangladesh';
+console.log(country);
+
+
+
+
+
+// boolean
+var isRich=true;
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log( typeof country);
+
+
+
+// number
+var price = 555;
+console.log(price);

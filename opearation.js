fb5aded097c1760c6c1c77038d93cd53b4fefce8
coder-utshab <ghoshutshob470@gmail.com>
2026-01-02 00:43:33 +0530

    up

---
+++ opearation.js
@@ -0,0 +1,11 @@
+var applePrice = 50;
+var mangoPrice = 40;
+
+// console.log(mangoPrice + applePrice);
+// console.log(mangoPrice - applePrice);
+// console.log(mangoPrice * applePrice);
+// console.log(mangoPrice / applePrice);
+
+
+console.log(10/3);
+console.log(10%3);

6afabeb4bdaf950eaffaf259bb3589500611417e
coder-utshab <ghoshutshob470@gmail.com>
2026-01-02 22:49:21 +0530

    up

---
+++ sortend.js
@@ -0,0 +1,7 @@
+const isLeader = true;
+if(isLeader){
+    console.log('bangladesh is a small country');
+}
+else{
+    console.log('my name is a utshab kumar ghosh');
+}
---
+++ ternary.js
@@ -0,0 +1,29 @@
+// TERNARY----> Threeparts
+// normal if else
+// const age = 12;
+// if(age >= 18){
+//     console.log('You can vote.')
+// }
+// else{
+//     console.log('ghumai thako')
+// }
+
+
+
+// simple ternary
+const age = 120;
+age >= 18 ? console.log('vote deo') : console.log('ghumay thako')
+
+
+
+
+let price = 500;
+const isLeader = false;
+
+if(isLeader === true){
+    price = 0;
+}
+else{
+    price = price + 100;
+}
+console.log(price)

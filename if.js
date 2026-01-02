939bca9122f66aa6d5268835be822fc1cd5ffca4
coder-utshab <ghoshutshob470@gmail.com>
2026-01-02 20:12:41 +0530

    up

---
+++ if.js
@@ -0,0 +1,26 @@
+//  if(condition){
+
+//  }
+
+
+if(13 < 20){
+    console.log('i have a smaller value');
+}
+
+
+var biryaniPrice = 200
+if(biryaniPrice < 300){
+    console.log('mama give me some biryani')
+}
+
+
+
+
+var weight = 10;
+
+if(weight < 20){
+    console.log('i will carry it by myself');
+}
+else{
+    console.log('i will rent a rickshaw');
+}
---
+++ multiple.js
@@ -0,0 +1,10 @@
+const salary = 25000;
+const isBCS = true;
+const height = 61;
+
+if(salary > 20000 && height > 66){ 
+    console.log('my name is utshab');
+}
+else{
+    console.log('studey in parul university');
+}

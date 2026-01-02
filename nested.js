32ffe8b0f8015020401114e2968d220b1bf585b6
coder-utshab <ghoshutshob470@gmail.com>
2026-01-02 21:54:04 +0530

    up

---
+++ nested.js
@@ -0,0 +1,17 @@
+const money = 10;
+if (money > 300){
+    console.log('bro you are rich!!');
+}
+else{
+    if(money > 100){
+        console.log('tui gorib o nahh boro lok o nah')
+    }
+    else{
+        if (money > 0){
+            console.log('dosto kola kha calcium bara')
+        }
+        else {
+            console.log('tui amr bondhu')
+        }
+    }
+}
